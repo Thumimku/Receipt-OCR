fc215c4d36c90f46875ab0685024e660865abbe7
thumimku <thumilan.15@cse.mrt.ac.lk>
2019-03-21 12:32:46 +0530

    Initiative

---
+++ Tesseract/pythontesseract.py
@@ -0,0 +1,75 @@
+import tempfile
+
+import imutils as imutils
+from PIL import Image
+import pytesseract
+import cv2
+import numpy as np
+import os.path
+
+
+def pre_processing(img_path):
+    img = cv2.imread(img_path)
+    img = cv2.resize(img, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
+    # Convert to gray
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Apply dilation and erosion to remove some noise
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+
+    # Apply blur to smooth out the edges
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+    return
+
+def save_file(data_text,file_name):
+    file_object = open(file_name, "w")
+    file_object.write(data_text)
+    file_object.close()
+
+def set_image_dpi(file_path):
+    im = Image.open(file_path)
+    length_x, width_y = im.size
+    factor = min(1, float(1024.0 / length_x))
+    size = int(factor * length_x), int(factor * width_y)
+    im_resized = im.resize(size, Image.ANTIALIAS)
+    temp_file = tempfile.NamedTemporaryFile(delete=False,   suffix='.png')
+    temp_filename = temp_file.name
+    im_resized.save(temp_filename, dpi=(300, 300))
+    return temp_filename
+
+def detect_text_block_with_skew(file_name):
+    image = cv2.imread(file_name)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(gray, 10, 50)
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+    cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
+    screenCnt = None
+    for c in cnts:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        if len(approx) == 4:
+            screenCnt = approx
+            break
+
+    cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+
+# def remove_noise_and_smooth(file_name):
+#     img = cv2.imread(file_name, 0)
+#     filtered = cv2.adaptiveThreshold(img.astype(np.uint8), 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 41)
+#     kernel = np.ones((1, 1), np.uint8)
+#     opening = cv2.morphologyEx(filtered, cv2.MORPH_OPEN, kernel)
+#     closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
+#     img = image_smoothening(img)
+#     or_image = cv2.bitwise_or(img, closing)
+#     return or_image
+
+save_path = "/home/thumilan/Desktop/FYP/Evaluation"
+completeName = os.path.join(save_path, "output"+".txt")
+data_text = pytesseract.image_to_string(Image.open(set_image_dpi('test.png')), lang='fra')
+save_file(data_text,completeName)
+
+# detect_text_block_with_skew('/home/thumilan/Desktop/FYP/Tesseract/test.png')
